2c271ebfcb6fbb43fde1d2a0677bd68cc4847d7d
Kazimir <kazimirdev@tuta.io>
2025-07-15 20:26:45 +0200

    image scaler; rm shadow from preview

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# Configure your database URI. Example with SQLite:
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///osk.db'
 db = SQLAlchemy(app)
 
